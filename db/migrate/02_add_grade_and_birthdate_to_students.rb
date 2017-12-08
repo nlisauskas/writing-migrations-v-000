a545769a83bd26f5da2a55a940c98f09bf061950
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-08 14:57:29 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
     add_column do |t|
-      t.integer :grade
+      t.integer :grade,
       t.string :birthdate
     end
   end
